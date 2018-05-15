d61507f273d45e46548982f0060b39074b210c3c
Markus Sommer <markus@splork.de>
2018-05-15 14:14:27 +0200

    Some keyring-work I forgot to commit...

--- examples/keyring.py
+++ examples/keyring.py
@@ -5,27 +5,28 @@ import argparse
 
 from pyserval.lowlevel.client import ServalClient
 
-parser = argparse.ArgumentParser(description="Test keyring-endpoints")
-parser.add_argument('-ho',
-                    '--host',
-                    default='localhost',
-                    help='Host running the servald REST-interface')
-parser.add_argument('-po',
-                    '--port',
-                    default=4110,
-                    help='Port exposing the servald REST-interface')
-parser.add_argument('-u',
-                    '--user',
-                    default='pum',
-                    help='Username for authentication to the REST interface')
-parser.add_argument('-pw',
-                    '--password',
-                    default='pum123',
-                    help='Password for authentication to the REST interface')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Test keyring-endpoints")
+    parser.add_argument('-ho',
+                        '--host',
+                        default='localhost',
+                        help='Host running the servald REST-interface')
+    parser.add_argument('-po',
+                        '--port',
+                        default=4110,
+                        help='Port exposing the servald REST-interface')
+    parser.add_argument('-u',
+                        '--user',
+                        default='pum',
+                        help='Username for authentication to the REST interface')
+    parser.add_argument('-pw',
+                        '--password',
+                        default='pum123',
+                        help='Password for authentication to the REST interface')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-c = ServalClient(host=args.host, port=args.port, user=args.user, passwd=args.password)
+    c = ServalClient(host=args.host, port=args.port, user=args.user, passwd=args.password)
 
-print("Identities:")
-print(c.keyring.get_identities())
+    print("Identities:")
+    print(c.keyring.get_identities())
--- src/pyserval/highlevel/__init__.py
+++ src/pyserval/highlevel/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ["highlevel_client", "highlevel_keyring"]
+__all__ = ["highlevel_client", "highlevel_keyring", "highlevel_rhizome"]
