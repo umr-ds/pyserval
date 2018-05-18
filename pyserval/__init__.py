0d9cebf5e9baf11bb02802bf33dc959cdb445607
Markus Sommer <markus@splork.de>
2018-05-18 15:03:14 +0200

    Proper Packaging
    now compliant with https://packaging.python.org/tutorials/distributing-packages/

--- pyserval/__init__.py
+++ pyserval/__init__.py
@@ -2,7 +2,7 @@
 __all__ = ["highlevel", "lowlevel", "exceptions"]
 
 __title__ = 'pyserval'
-__version__ = '0.1.2'
+__version__ = '0.2'
 __author__ = u'Jonas Höchst & Markus Sommer'
 __license__ = 'MIT'
 __copyright__ = u'Copyright 2017 Jonas Höchst & Markus Sommer'
--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+"""Based on the pypa sample-project
 
+See: https://github.com/pypa/sampleproject
+"""
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 dependencies = [
     'requests',
 ]
-
 
 test_dependencies = [
     'pytest',
@@ -18,32 +26,35 @@ test_dependencies = [
     'pytest-cov'
 ]
 
-
 extras = {
     'test': test_dependencies,
 }
 
-
-setup(name='pyserval',
-      version='0.2',
-      description='Python client for serval-dns REST interface',
-      long_description=readme(),
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-      ],
-      url='https://github.com/umr-ds/pyserval',
-      author=u'Jonas Höchst & Markus Sommer',
-      author_email='msommer@informatik.uni-marburg.de',
-      license='MIT',
-      packages=['pyserval'],
-      package_dir={'': 'src'},
-      install_requires=dependencies,
-      tests_require=test_dependencies,
-      extras_require=extras,
-      include_package_data=True,
-      zip_safe=True)
+setup(
+    name='pyserval',
+    version='0.2',
+    description='Python client for serval-dna REST interface',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/umr-ds/pyserval',
+    author=u'Jonas Höchst & Markus Sommer',
+    author_email='msommer@informatik.uni-marburg.de',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='serval dtn',
+    license='MIT',
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=dependencies,
+    extras_require=extras,
+    zip_safe=True,
+    project_urls={
+        'Bug Reports': 'https://github.com/umr-ds/pyserval/issues',
+        'Source': 'https://github.com/umr-ds/pyserval',
+    }
+)
