b82f50a9463185cc2c6a25394631754c7ff3ced0
Markus Sommer <markus@splork.de>
2018-10-23 11:19:31 +0200

    Update metadata for release

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ See: https://github.com/pypa/sampleproject
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -13,37 +14,36 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-dependencies = [
-    'requests',
-]
+dependencies = ["requests"]
 
 setup(
-    name='pyserval',
-    version='0.3.2',
-    description='Python client for serval-dna REST interface',
+    name="pyserval",
+    version="0.3.3",
+    description="Python client for serval-dna REST interface",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/umr-ds/pyserval',
-    author=u'Jonas Höchst & Markus Sommer',
-    author_email='msommer@informatik.uni-marburg.de',
+    long_description_content_type="text/markdown",
+    url="https://github.com/umr-ds/pyserval",
+    author=u"Jonas Höchst & Markus Sommer",
+    author_email="msommer@informatik.uni-marburg.de",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    keywords='serval dtn',
-    license='MIT',
-    packages=find_packages(exclude=['docs', 'tests', 'examples']),
+    keywords="serval dtn",
+    license="MIT",
+    packages=find_packages(exclude=["docs", "tests", "examples"]),
     install_requires=dependencies,
     zip_safe=True,
     project_urls={
-        'Bug Reports': 'https://github.com/umr-ds/pyserval/issues',
-        'Source': 'https://github.com/umr-ds/pyserval',
-    }
+        "Bug Reports": "https://github.com/umr-ds/pyserval/issues",
+        "Source": "https://github.com/umr-ds/pyserval",
+    },
 )
