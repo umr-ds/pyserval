9168c6efbc6da7162712409c2c20549f93add9ac
Markus Sommer <msommer@informatik.uni-marburg.de>
2019-09-30 15:46:30 +0200

    Update metadata for release

--- pyserval/__init__.py
+++ pyserval/__init__.py
@@ -12,7 +12,7 @@ __all__ = [
 ]
 
 __title__ = "pyserval"
-__version__ = "0.4"
+__version__ = "0.4.1"
 __author__ = u"Jonas Höchst & Markus Sommer"
 __license__ = "MIT"
 __copyright__ = u"Copyright 2017 Jonas Höchst & Markus Sommer"
